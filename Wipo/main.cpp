813b004b5f6225d8e412aef6fb62dbf2ed222a31
xtuer <biao.mac@icloud.com>
2017-01-04 09:02:41 +0800

    config

--- Wipo/main.cpp
+++ Wipo/main.cpp
@@ -10,9 +10,7 @@ int main(int argc, char *argv[]) {
     UiUtil::loadQss();
 
     MainWidget w;
-    QRect desktopRect = QApplication::desktop()->availableGeometry();
-    QPoint center = desktopRect.center();
-    w.move(center.x()-w.width()*0.5, center.y()-w.height()*0.5);
+    w.move(QApplication::desktop()->availableGeometry().center() - w.rect().center()); // 居中
     w.show();
 
     return a.exec();
