f58974c1acf57bac937b79c913a838e8d0e964c8
xtuer <biao.mac@icloud.com>
2017-08-11 13:21:25 +0800

    拍照上传

--- Gui/main.cpp
+++ Gui/main.cpp
@@ -1,11 +1,12 @@
 #include <QApplication>
-#include "Widget.h"
+#include <iostream>
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 
-    Widget w;
-    w.show();
+    QString str("Hello 中国");
+    std::string foo = str.toStdString();
+    std::cout << foo << std::endl;
 
     return a.exec();
 }
