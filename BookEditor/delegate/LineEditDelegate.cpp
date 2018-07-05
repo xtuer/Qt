4f2731c6dd93fe8a1d09c87223d44d3c386a2c4e
Biao <biao.mac@icloud.com>
2018-07-05 15:03:23 +0800

    fuck

--- BookEditor/delegate/LineEditDelegate.cpp
+++ BookEditor/delegate/LineEditDelegate.cpp
@@ -7,5 +7,6 @@ LineEditDelegate::LineEditDelegate(QObject *parent) : QStyledItemDelegate(parent
 void LineEditDelegate::updateEditorGeometry(QWidget *editor,
                                             const QStyleOptionViewItem &option,
                                             const QModelIndex &index) const {
+    Q_UNUSED(index)
     editor->setGeometry(option.rect);
 }
