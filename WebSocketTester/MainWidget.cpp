fef26fd8b24b4c3ad7dcb05a7387d2c6951f8c85
Biao <biao.mac@icloud.com>
2019-01-08 11:43:50 +0800

    Comments

--- WebSocketTester/MainWidget.cpp
+++ WebSocketTester/MainWidget.cpp
@@ -9,11 +9,13 @@
 MainWidget::MainWidget(QWidget *parent) : QWidget(parent), ui(new Ui::MainWidget) {
     ui->setupUi(this);
 
+    // 点击开始按钮, 连接服务器
     connect(ui->startButton, &QPushButton::clicked, [this] {
         ui->startButton->setDisabled(true);
         QString url = ui->urlLineEdit->text().trimmed();
         int amount = ui->amountSpinBox->value();
 
+        // 创建 amount 个连接
         for (int i = 1; i <= amount; ++i) {
             QString uuid = QUuid::createUuid().toString().toUpper();
 
@@ -24,11 +26,13 @@ MainWidget::MainWidget(QWidget *parent) : QWidget(parent), ui(new Ui::MainWidget
             QWebSocket *socket = new QWebSocket();
             QTimer *timer = new QTimer();
 
+            // 连接成功过
             connect(socket, &QWebSocket::connected, [=] {
                 connectedCount++;
                 ui->connectedCountLabel->setText(QString("已建立连接: %1").arg(connectedCount));
-                timer->start(5000);
+                timer->start(5000); // 启动定时器, 开始发送心跳
             });
+            // 连接断开
             connect(socket, &QWebSocket::disconnected, [this] {
                 connectedCount--;
                 ui->connectedCountLabel->setText(QString("已建立连接: %1").arg(connectedCount));
@@ -37,7 +41,7 @@ MainWidget::MainWidget(QWidget *parent) : QWidget(parent), ui(new Ui::MainWidget
                     ui->startButton->setEnabled(true);
                 }
             });
-            // 发送心跳
+            // 定时发送心跳
             connect(timer, &QTimer::timeout, [=] {
                 socket->sendTextMessage("{\"type\": \"HEARTBEAT\"}");
             });
